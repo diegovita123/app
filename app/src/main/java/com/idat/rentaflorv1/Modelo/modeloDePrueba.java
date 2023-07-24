a31983c7b2d6e7ce6ac18ca5bad2b06608840e87
diegovita123 <cesarvita05@gmail.com>
2023-07-24 09:49:30 -0500

    terminamos la clase modelo

--- app/src/main/java/com/idat/rentaflorv1/Modelo/modeloDePrueba.java
+++ app/src/main/java/com/idat/rentaflorv1/Modelo/modeloDePrueba.java
@@ -1,5 +1,11 @@
 package com.idat.rentaflorv1.Modelo;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
+
 public class modeloDePrueba {
     private int codigo;
     private String nombre;
@@ -18,5 +24,24 @@ public class modeloDePrueba {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public modeloDePrueba() {
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof modeloDePrueba)) return false;
+        modeloDePrueba that = (modeloDePrueba) o;
+        return getCodigo() == that.getCodigo() &&
+                Objects.equals(getNombre(), that.getNombre());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo(), getNombre());
     }
 }
