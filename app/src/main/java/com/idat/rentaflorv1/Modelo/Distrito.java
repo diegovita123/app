217c67b373ad2cb060e95067a716b92bd237d9ea
dany-rodriguez <u17210636@utp.edu.pe>
2023-07-25 22:51:12 -0500

    modelo distrito terminado

--- app/src/main/java/com/idat/rentaflorv1/Modelo/Distrito.java
+++ app/src/main/java/com/idat/rentaflorv1/Modelo/Distrito.java
@@ -3,7 +3,65 @@ package com.idat.rentaflorv1.Modelo;
 public class Distrito {
     private int id;
     private String descripcion;
+private int id;
+    private String descripcion;
+    private int id;
+    private String descripcion;
+    private int id;
+    private String descripcion;
+    private int id;
+    private String descripcion;
+    private int id;
+    private String descripcion;
 
+    public Camioneta() {
+    }
+
+    public Camioneta(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+    public String getDescripcion() {
+        return id;
+    }
+    public String getDescripcion() {
+        return nombre;
+    }
+    public String getDescripcion() {
+        return th;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Provincia() {
+    }
+
+    public Provincia(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     public Distrito() {
     }
 
